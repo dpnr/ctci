c6d97f578b8a743786a5034c2c01601f8920d0a2
pranavdommata <pranav.dommata@ubnt.com>
2019-12-01 20:03:28 -0600

    introduce the linkedlist problems

---
+++ ctci-practice/src/main/java/com/ctci/practice/linkedlists/LinkedlistProblems.java
@@ -0,0 +1,19 @@
+package com.ctci.practice.linkedlists;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class LinkedlistProblems {
+    public List<Integer> inputList;
+
+    public LinkedlistProblems(List<Integer> inputList) {
+        inputList = new LinkedList<>(inputList);
+        System.out.println("pnr sys-debug :: ==> " + "initialized the linked list ");
+        System.out.println("pnr sys-debug :: ==> " + inputList);
+    }
+
+    public static void run() {
+
+    }
+}
---
+++ ctci-practice/src/test/java/com/ctci/practice/linkedlists/LinkedListsProblemsTest.java
@@ -0,0 +1,19 @@
+package com.ctci.practice.linkedlists;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
+
+/**
+ * PermutationsPalindromeTest
+ */
+public class LinkedListsProblemsTest {
+
+    @Test
+    public void testEmptyString() {
+        String empty = "";
+        LinkedlistProblems ll = new LinkedlistProblems(Arrays.asList(new Integer[]{1,2,3}));
+    }
+}
